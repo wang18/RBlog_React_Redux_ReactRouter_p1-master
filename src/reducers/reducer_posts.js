d35bce8391fde3336d745c791e9714eb5214daae
wang18 <wang18@uoregon.edu>
2018-01-03 14:45:55 -0800

    complete project

---
+++ src/reducers/reducer_posts.js
@@ -0,0 +1,16 @@
+import {FETCH_POSTS, FETCH_POST, DELETE_POST} from '../actions/index';
+import _ from 'lodash';
+
+export default function (state={}, action) {
+    switch (action.type){
+        case FETCH_POSTS:
+            return _.mapKeys(action.payload.data, 'id');
+        case FETCH_POST:
+            const post = action.payload.data;
+            return {...state, [action.payload.data.id]: action.payload.data};
+        case DELETE_POST:
+            return _.omit(state, action.payload);
+        default:
+            return state;
+    }
+}
